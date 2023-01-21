d931047db5efe547583cadb05bfd0089b9ec775b
abolfazl7710 <121865579+abolfazl7710@users.noreply.github.com>
2023-01-21 15:37:07 +0330

    Update 3_Nft.py

--- pages/3_Nft.py
+++ pages/3_Nft.py
@@ -11,7 +11,7 @@ st.set_page_config(
 st.title("🌃 NFT (minting & selling activity)")
 st.sidebar.success("🌃 NFT (minting & selling activity)")
 
-
+@st.cache(ttl=10000)
 def querying_pagination(query_string):
     sdk = ShroomDK('8c37dc3a-fcf4-42a1-a860-337fa9931a2a')
     result_list = []
